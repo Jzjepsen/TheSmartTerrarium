70c5f30c0e9ab65a61dfb9ac3d2f681f2b14b677
Jzjepsen <jzjepsen@gmail.com>
2023-10-27 11:50:15 +0200

    Same

--- dataGenerator.js
+++ dataGenerator.js
@@ -3,10 +3,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var casual = require("casual");
 setInterval(function () {
     var temperature = casual.integer(20, 35); // range for terrarium temperature in Celsius  
-    var moisture = casual.integer(50, 100); // range for terrarium moisture in percentage  
+    var humidity = casual.integer(50, 100); // range for terrarium humidity in percentage  
     var data = {
         temperature: temperature,
-        moisture: moisture
+        humidity: humidity
     };
     console.log(JSON.stringify(data));
 }, 10000);
